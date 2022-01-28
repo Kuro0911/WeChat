7f5db9432a96e20cd910a2c828aff9bd8c7bd3be
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-01-28 18:44:40 +0530

    geting data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
 
 function App() {
+  useEffect(() => {
+    var pusher = new Pusher("ca483be98c5f58141de1", {
+      cluster: "ap2",
+    });
+
+    var channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app-body">

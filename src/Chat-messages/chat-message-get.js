753a7c0eaa2ca1cd1bac2d579ebb3254c510f4f7
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2022-01-28 15:19:38 +0530

    fixes

--- src/Chat-messages/chat-message-get.js
+++ src/Chat-messages/chat-message-get.js
@@ -4,7 +4,7 @@ function ChatMessageGet() {
   return (
     <p className="chat-get">
       <span className="chat-name">kuro</span>
-      this is what i recive suiii
+      this is what i GET suiii
       <span className="chat-time">
         {new Date().toLocaleTimeString("en-US", {
           hour12: false,
